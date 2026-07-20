2a86fa26a983158c6eb831ee0b07ad89ebd6781f
Amulya <Amulya@MSI>
2026-07-20 12:40:28 +0530

    first commit

---
+++ TNSIF_JAVA/src/org/tnsif/acc/c2tc/sr_developer/KIA_Internal.java
@@ -0,0 +1,11 @@
+package org.tnsif.acc.c2tc.sr_developer;
+import jr_developer.*;
+public class KIA_Internal {
+    public static void main(String[] args) {
+	KIA_Airbags airbag=new KIA_Airbags();
+	airbag.disp_airbags();
+	
+
+}
+
+}
