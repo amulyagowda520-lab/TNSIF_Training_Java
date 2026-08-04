fe2f013bf7979438c1db72e5575f030140bea776
Amulya <Amulya@MSI>
2026-08-04 12:20:36 +0530

    first commit

---
+++ TNSIF_JAVA/src/org/tnsif/acc/c2tc/scannermethod_buffermethod/ScannerMethodDemo1.java
@@ -0,0 +1,19 @@
+package org.tnsif.acc.c2tc.scannermethod_buffermethod;
+import java.util.*;
+public class ScannerMethodDemo1 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the employee name");
+		String name=sc.nextLine();
+		System.out.println("Enter the employee ID");
+		int ID=sc.nextInt();
+		System.out.println("Student_name:"+name);
+		System.out.println("Enter the student USN:"+ ID);
+		
+		
+		
+
+	}
+
+}
