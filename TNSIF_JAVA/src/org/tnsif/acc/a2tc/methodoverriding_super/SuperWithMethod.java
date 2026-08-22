da97d42168eea206733330b62e374e97825690df
Amulya <Amulya@MSI>
2026-08-22 19:45:32 +0530

    first commit

---
+++ TNSIF_JAVA/src/org/tnsif/acc/a2tc/methodoverriding_super/SuperWithMethod.java
@@ -0,0 +1,31 @@
+package org.tnsif.acc.a2tc.methodoverriding_super;
+
+
+class Payment1
+{
+	void process()
+	{
+		System.out.println("Processing payment using standard gateway");
+	}
+}
+class GPay extends Payment1
+{
+	void process()
+	{
+		System.out.println("Processing payment via google pay");
+	}
+	void completeTransaction()
+	{
+		super.process();
+		process();
+	}
+}
+public class SuperWithMethod {
+
+	public static void main(String[] args) {
+		GPay obj=new GPay();
+		obj.completeTransaction();
+
+	}
+
+}
