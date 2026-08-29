b8bdb19d714a9ddd00f0a622a1015567b3db2be3
Amulya <Amulya@MSI>
2026-08-29 20:28:11 +0530

    first commit

--- TNSIF_JAVA/src/org/tnsif/acc/c2tc/interfacedemo/FunctionalInterfaceDemo.java
+++ TNSIF_JAVA/src/org/tnsif/acc/c2tc/interfacedemo/FunctionalInterfaceDemo.java
@@ -1,4 +1,4 @@
-package org.tnsif.acc.tnsif.interfacedemo;
+package org.tnsif.acc.c2tc.interfacedemo;
 
 @FunctionalInterface
 interface NotificationService
--- TNSIF_JAVA/src/org/tnsif/acc/c2tc/staticdemo_finaldemo/FinalOnMethod.java
+++ TNSIF_JAVA/src/org/tnsif/acc/c2tc/staticdemo_finaldemo/FinalOnMethod.java
@@ -4,7 +4,7 @@ package org.tnsif.acc.c2tc.staticdemo_finaldemo;
 
 class Bank
 {
-	final void security_Rule()
+	
 	{
 		System.out.println(" Follow kyc and OTP verification");
 	}
