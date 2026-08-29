920a06d07177ffb382bad79c905ba44763e153b6
Amulya <Amulya@MSI>
2026-08-29 19:01:18 +0530

    first commit

---
+++ TNSIF_JAVA/src/org/tnsif/acc/tnsif/interfacedemo/SmartDevice.java
@@ -0,0 +1,12 @@
+package org.tnsif.acc.tnsif.interfacedemo;
+
+
+
+public interface SmartDevice { 
+	
+	
+	public abstract void turnOn();
+	public abstract void turnOff();
+	public abstract void getStatus();
+
+}
