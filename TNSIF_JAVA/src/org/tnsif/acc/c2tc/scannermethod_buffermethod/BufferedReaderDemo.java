d8071b410c6d5d709f90197ca78b0c9e274a43bd
Amulya <Amulya@MSI>
2026-08-29 19:43:43 +0530

    first commit

---
+++ TNSIF_JAVA/src/org/tnsif/acc/c2tc/scannermethod_buffermethod/BufferedReaderDemo.java
@@ -0,0 +1,10 @@
+package org.tnsif.acc.c2tc.scannermethod_buffermethod;
+
+public class BufferedReaderDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
